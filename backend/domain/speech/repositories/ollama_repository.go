6e46e35595200aad098ab7a9673e46714ebf1f17
farismnrr <farismunir2@gmail.com>
2026-01-27 09:35:17 +0700

    OLLAMA: fallback to host.docker.internal if localhost is unreachable from container

--- backend/domain/speech/repositories/ollama_repository.go
+++ backend/domain/speech/repositories/ollama_repository.go
@@ -5,16 +5,59 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"net/url"
 	"strings"
+	"time"
+
+	"teralux_app/domain/common/utils"
 )
 
 type OllamaRepository struct {
 	baseURL string
 }
 
+func tryDial(host, port string) error {
+	addr := net.JoinHostPort(host, port)
+	conn, err := net.DialTimeout("tcp", addr, 500*time.Millisecond)
+	if err != nil {
+		return err
+	}
+	_ = conn.Close()
+	return nil
+}
+
 func NewOllamaRepository() *OllamaRepository {
-	return &OllamaRepository{baseURL: "http://localhost:11434"}
+	cfg := utils.GetConfig()
+	base := cfg.OllamaURL
+	if base == "" {
+		base = "http://localhost:11434"
+	}
+
+	// If configured host is localhost/127.0.0.1 but not reachable from container,
+	// try host.docker.internal:PORT as fallback (useful when running inside Docker).
+	if u, err := url.Parse(base); err == nil {
+		host := u.Hostname()
+		port := u.Port()
+		if port == "" {
+			if u.Scheme == "http" {
+				port = "80"
+			} else if u.Scheme == "https" {
+				port = "443"
+			}
+		}
+		if (host == "localhost" || host == "127.0.0.1") && tryDial(host, port) != nil {
+			alt := "host.docker.internal"
+			if tryDial(alt, port) == nil {
+				u.Host = alt + ":" + port
+				base = u.String()
+				utils.LogInfo("OLLAMA host %s not reachable from container; switching to %s", host, alt)
+			}
+		}
+	}
+
+	return &OllamaRepository{baseURL: base}
 }
 
 func (r *OllamaRepository) CallModel(prompt string, model string) (string, error) {
